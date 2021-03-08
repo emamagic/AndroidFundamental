fd8a05e938c907257038ff3fceded1901e03e1d7
developer developer <developermagic7@gmail.com>
2021-03-08 01:34:59 -0800

    step_1

--- app/src/main/java/com/example/codinginflow/MainActivity.kt
+++ app/src/main/java/com/example/codinginflow/MainActivity.kt
@@ -9,41 +9,19 @@ import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(toolbar)
-
-        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-        nav_view.setNavigationItemSelectedListener(this)
-
-        // drawer hold last state of selected item while configuration change and we do not want miss the state we configuration changed
-        if (savedInstanceState == null) nav_view.setCheckedItem(R.id.drawer_1)
-
-
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) drawer_layout.closeDrawer(GravityCompat.START)
-        else super.onBackPressed()
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.drawer_1 -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
-            R.id.drawer_2 -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
-            R.id.drawer_3 -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
-            R.id.drawer_4 -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
-            R.id.drawer_5 -> Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        btn.setOnClickListener {
+            MyBottomSheetDialog().show(supportFragmentManager ,null)
         }
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
+
     }
+
+
 
 
 }
