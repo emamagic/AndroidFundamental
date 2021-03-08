bb0855e59a03da1b9a12e2703daacbaed91e895e
developer developer <developermagic7@gmail.com>
2021-03-08 04:37:13 -0800

    step_1

--- app/src/main/java/com/example/codinginflow/MainActivity.kt
+++ app/src/main/java/com/example/codinginflow/MainActivity.kt
@@ -11,18 +11,11 @@ import com.example.codinginflow.databinding.FragmentMain1Binding
 
 class MainActivity : AppCompatActivity()  {
 
-    private lateinit var fragment1: MyFragment_1
-    private lateinit var fragment2: MyFragment_2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null){
-            fragment1 = MyFragment_1()
-            fragment2 = MyFragment_2()
-            supportFragmentManager.beginTransaction().replace(R.id.container_fragment_1 ,fragment1).replace(R.id.container_fragment_2 ,fragment2).commit()
-        }
 
     }
 
