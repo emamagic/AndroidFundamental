ea7d056bd199f0c2f050655cf8eaa2fe0e46eeb5
developer developer <developermagic7@gmail.com>
2021-03-07 23:13:05 -0800

    step_1

--- app/src/main/java/com/example/codinginflow/MainActivity.kt
+++ app/src/main/java/com/example/codinginflow/MainActivity.kt
@@ -2,34 +2,15 @@ package com.example.codinginflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import com.example.codinginflow.databinding.ActivityMainBinding
-import com.example.codinginflow.databinding.FragmentMain1Binding
 
-class MainActivity : AppCompatActivity() ,MyFragment_1.OnFragment_1 ,MyFragment_2.OnFragment_2 {
-
-    private lateinit var fragment1: MyFragment_1
-    private lateinit var fragment2: MyFragment_2
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fragment1 = MyFragment_1()
-        fragment2 = MyFragment_2()
-        supportFragmentManager.beginTransaction().replace(R.id.container_fragment_1 ,fragment1).replace(R.id.container_fragment_2 ,fragment2).commit()
     }
 
-    override fun data_1(text: String) {
-        Toast.makeText(this, "sss", Toast.LENGTH_SHORT).show()
-       fragment2.setText_2(text)
-    }
 
-    override fun data_2(message: String) {
-        fragment1.setText_1(message)
-    }
 
 
 }
--- app/src/main/java/com/example/codinginflow/MyFragment_1.kt
+++ app/src/main/java/com/example/codinginflow/MyFragment_1.kt
@@ -1,11 +1,9 @@
 package com.example.codinginflow
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.codinginflow.databinding.FragmentMain1Binding
 
@@ -26,6 +24,8 @@ class MyFragment_1: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (parentFragment is MyFragment_1.OnFragment_1) listener = parentFragment as OnFragment_1
         binding?.btnFragment1?.setOnClickListener { listener?.data_1(binding?.txtFragment1?.text.toString()) }
     }
 
@@ -37,12 +37,6 @@ class MyFragment_1: Fragment() {
         fun data_1(text: String)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragment_1){
-            listener = context
-        }
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
