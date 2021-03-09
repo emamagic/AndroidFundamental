5f5899a3fe50e3e31a9aa94e0ff841f82c18d001
developer developer <developermagic7@gmail.com>
2021-03-08 20:50:43 -0800

    step_1

--- app/src/main/java/com/example/codinginflow/MainActivity.kt
+++ app/src/main/java/com/example/codinginflow/MainActivity.kt
@@ -4,34 +4,31 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AlertDialog
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.view.GravityCompat
 import com.example.codinginflow.Constance.BROAD_CAST_KET
 import com.example.codinginflow.Constance.CHANNEL_1_ID
 import com.example.codinginflow.Constance.CHANNEL_2_ID
-import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var manager: NotificationManagerCompat
+    private lateinit var mediaSession: MediaSessionCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         manager = NotificationManagerCompat.from(this)
+        mediaSession = MediaSessionCompat(this ,"tag")
 
         btn_notification_1.setOnClickListener {
 
-            val largeIcon = BitmapFactory.decodeResource(resources ,R.drawable.common_full_open_on_phone)
+            val picture = BitmapFactory.decodeResource(resources ,R.drawable.common_full_open_on_phone)
 
             // we have config same as channel in application class for api 25 or lower
             val notification = NotificationCompat.Builder(this ,CHANNEL_1_ID)
@@ -42,38 +39,38 @@ class MainActivity : AppCompatActivity() {
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                     .setColor(Color.BLUE)
-                    .setLargeIcon(largeIcon)
-                    .setStyle(NotificationCompat.BigTextStyle()
-                            .bigText(getString(R.string.dummy_string))
-                            .setBigContentTitle("Big Content Title")
-                            .setSummaryText("Summery Text"))
+                    .setLargeIcon(picture)
+                    .setStyle(NotificationCompat.BigPictureStyle()
+                            .bigPicture(picture)
+                            .bigLargeIcon(null))
                     .setContentIntent(openActivity())
                     .setAutoCancel(true)
                     .setOnlyAlertOnce(true)
-                    .addAction(R.mipmap.ic_launcher ,"Toast" ,triggeredBroadCast(edt_notification_message.text.toString()))
                     .build()
             // if we send multiple notification with same id it override on it
             manager.notify(1 ,notification)
         }
 
         btn_notification_2.setOnClickListener {
+
+            val artwork = BitmapFactory.decodeResource(resources ,R.drawable.common_full_open_on_phone)
+
+
             // we have config same as channel in application class for api 25 or lower
             val notification = NotificationCompat.Builder(this ,CHANNEL_2_ID)
                     .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
                     .setContentText(edt_notification_message.text.toString())
                     .setContentTitle(edt_notification_title.text.toString())
                     .setPriority(NotificationCompat.PRIORITY_LOW)
-                    .setStyle(NotificationCompat.InboxStyle()
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .addLine("This is line 1")
-                            .setBigContentTitle("Big Content Title")
-                            .setSummaryText("Summery Text"))
+                    .setLargeIcon(artwork)
+                    .addAction(R.drawable.common_google_signin_btn_icon_light ,"dislike" ,null)
+                    .addAction(R.drawable.common_google_signin_btn_icon_light ,"dislike" ,null)
+                    .addAction(R.drawable.common_google_signin_btn_icon_light ,"dislike" ,null)
+                    .addAction(R.drawable.common_google_signin_btn_icon_light ,"dislike" ,null)
+                    .setStyle(androidx.media.app.NotificationCompat.MediaStyle()
+                            .setShowActionsInCompactView(1 ,2 ,3)
+                            .setMediaSession(mediaSession.sessionToken))
+                    .setSubText("sub text")
                     .build()
             // if we send multiple notification with same id it override on it
             manager.notify(2 ,notification)
