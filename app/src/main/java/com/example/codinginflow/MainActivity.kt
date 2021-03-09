9b09e8474f3861dd0657474a9cd9c7877e2340a1
developer developer <developermagic7@gmail.com>
2021-03-08 22:56:54 -0800

    step_1

--- app/src/main/java/com/example/codinginflow/MainActivity.kt
+++ app/src/main/java/com/example/codinginflow/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        float.setOnClickListener {
+        floate.setOnClickListener {
             Snackbar.make(it ,"snackbar" ,2000).show()
         }
 
