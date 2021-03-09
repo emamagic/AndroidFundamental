769bdb8af20c3b729e9d88c109f8e2d8ba5700b1
developer developer <developermagic7@gmail.com>
2021-03-08 23:17:37 -0800

    step_1

--- app/src/main/java/com/example/codinginflow/MainActivity.kt
+++ app/src/main/java/com/example/codinginflow/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.codinginflow
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -11,18 +16,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()  {
 
+    private lateinit var broadcastReceiver: BroadcastReceiver
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        floate.setOnClickListener {
-            Snackbar.make(it ,"snackbar" ,2000).show()
+
+
+
+    }
+
+    private fun startMinUpdate(){
+        val intentFilter = IntentFilter(Intent.ACTION_TIME_TICK)
+        broadcastReceiver = object :BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Toast.makeText(context, "hiii", Toast.LENGTH_SHORT).show()
+            }
         }
+        registerReceiver(broadcastReceiver ,intentFilter)
+    }
 
-        btn.setOnClickListener {
+    override fun onResume() {
+        super.onResume()
+        startMinUpdate()
+    }
 
-        }
-
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(broadcastReceiver)
     }
 }
